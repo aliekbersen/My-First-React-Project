c05c984759353ef3e588349502fa77e1db6ad36e
RexenN <96340446+RexenN@users.noreply.github.com>
2022-05-14 22:31:38 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-img
 import './App.css';
+import { useState } from 'react'
 
 function App() {
-let name = 'mario'
+const [showEvents, setShowEvents] = useState(true)
+const [events, setEvents] = useState([
+  {title: "mario's birthday bash", id: 1},
+  {title: "bowser's live stream", id: 2},
+  {title: "race on moo moo farm", id: 3}
+])
 
-const handleClick = () => {
-  name = 'luigi'
-  console.log(name)
+const handleClick = (id) => {
+  setEvents((prevEvents) => {
+    return prevEvents.filter((event) => {
+      return id !== event.id
+    })
+  })
+  console.log(id)
 }
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change Name</button>
+      {showEvents && (
+      <div>
+        <button onClick={() => setShowEvents(false)}>hide events</button>
+      </div>
+      )}
+      {!showEvents && (
+      <div>
+        <button onClick={() => setShowEvents(true)}>show events</button>
+      </div>
+      )}
+  
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+        <h2>{index} - {event.title}</h2>
+        <button onClick={() => handleClick(event.id)}>delete event</button>
+        </div>
+      ))}
     </div>
   );
 }
